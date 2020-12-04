0331abde7a89d19f47e0a05e97ff0b619222c006
Soraya Charef <b9015141@users.noreply.github.com>
2020-12-04 12:07:37 +0000

    fixed loop, added switch case

---
+++ OOP_Assignment/OOP Task 1a/Gate.h
@@ -0,0 +1,21 @@
+#pragma once
+class Gate
+{
+public:
+    // constructors
+    Gate() = default;
+    Gate(int x, int y);
+
+    // assessors
+    int GetX();
+    int GetY();
+    char GetSymbol() const;
+    bool IsAtPosition(int x, int y);
+
+private:
+    // data members
+    char symbol;
+    int x, y;
+    //initialise or remove empty constructor.
+};
+
